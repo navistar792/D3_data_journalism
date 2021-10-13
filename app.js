dfd73eeafca39e905264fc618dd0128899349601
navistar792 <r.verner99@yahoo.com>
2021-10-13 14:40:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -20,9 +20,12 @@ d3.csv(file).then(function(data) {
     console.log(hclow);
     var pov = data.map(d => d.poverty);
     console.log(pov);
+    var state = data.map(d => d.abbr);
+    console.log(state);
+
     // Add X axis
     const x = d3.scaleLinear()
-    .domain([2, 20])
+    .domain([8, 24])
     .range([ 0, width ]);
     svg.append("g")
     .attr("transform", `translate(0, ${height})`)
@@ -30,7 +33,7 @@ d3.csv(file).then(function(data) {
 
     // Add Y axis
     const y = d3.scaleLinear()
-    .domain([8, 23])
+    .domain([2, 26])
     .range([ height, 0]);
     svg.append("g")
     .call(d3.axisLeft(y));
@@ -40,9 +43,22 @@ d3.csv(file).then(function(data) {
     .selectAll("dot")
     .data(data)
     .join("circle")
-    .attr("cx", function (d) { return x(d.healthcareLow); } )
-    .attr("cy", function (d) { return y(d.poverty); } )
-        .attr("r", 6)
-        .style("fill", "#69b3a2")
+    .attr("cx", function (d) { return x(d.poverty); } )
+    .attr("cy", function (d) { return y(d.healthcareLow); } )
+        .attr("r", 12)
+        .style("fill", "#6699cc")
+    
+    svg.selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .style("font-size", "10px")
+        .attr("text-anchor", "middle")
+        // .attr("fill", "white")
+        // Add your code below this line
+                .text((d) => (d.abbr))
+                .attr("x", function (d) { return x(d.poverty);})
+                .attr("y", function (d) { return y(d.healthcareLow); })   
+        // Add your code above this line    
 
 });
